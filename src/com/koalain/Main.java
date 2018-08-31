2789fcfe19e64c00abb2f9f6588e3baaa03f2e48
tuyizhang <tuyizhang@126.com>
2018-08-31 16:30:08 +0800

    update

--- src/com/koalain/Main.java
+++
@@ -1,8 +0,0 @@
-package com.koalain;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/com/koalain/spring02/test/Test.java
+++ src/com/koalain/spring02/test/Test.java
@@ -1,4 +1,4 @@
-package com.koalain.spring02.Test;
+package com.koalain.spring02.test;
 
 import com.koalain.spring02.service.implement.UserServiceImplement;
 import org.springframework.context.ApplicationContext;
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args){
-        ApplicationContext context= new ClassPathXmlApplicationContext("beans02.xml");
+        ApplicationContext context= new ClassPathXmlApplicationContext("com/koalain/spring02/resource/beans02.xml");
         //get bean from factory
         UserServiceImplement userService = (UserServiceImplement)(context.getBean("userService"));
         userService.addUser();
